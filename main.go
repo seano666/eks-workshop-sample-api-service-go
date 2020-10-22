d37ed7feadcd0f57bade1c9f3f6e00ba5180137f
Sean Collins <mmmburger@gmail.com>
2020-10-22 14:56:03 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World2"}
+		res := &response{Message: "Hello World3"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
